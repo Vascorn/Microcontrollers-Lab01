d5d62e60cba06419a584946016248aea43150991
kprattis <prattiskonstantinos@gmail.com>
2023-04-05 19:28:25 +0300

    Updated tests
    Updated tests according to our final choice

--- Lab01/main.c
+++ Lab01/main.c
@@ -2,17 +2,16 @@
 #include <stdlib.h>
 #include "uart.h"
 
-
 #define MAXSIZE 100
 
-//#define DEBUG
+//#define DEBUG //uncomment this line to run the tests 
 #define TEST_HASH
 #define TEST_DRF
 
-/*! \brief calculate and return the hash of the input string given a table of values for each latin letter */
+/*! calculate and return the hash of the input string given a table of values for each latin letter */
 extern int hash(char* input, int* table);
 
-/*! \brief calculate and return the factorial of the digital root of a number */
+/*! calculate and return the factorial of the digital root of a number */
 extern int drootfactorial(int num);
 
 int main(void){
@@ -29,10 +28,11 @@ int main(void){
 	/* TESTING */
 	#ifdef DEBUG
 	int i, ntests, npassed, exit = 0;
-		
+	
+	//Tests for hash
 	#ifdef TEST_HASH
-	char *test_hash[] = {"Vasc0o Rizou 10021", "kprattis2001@ece.auth.gr", "ABCabc./*=&1", "T : 69***56483", "a55c66o77"};
-	int 	ans_hash[] 	= {					241				 ,					593							,				63			 ,				-41			,				0			};
+	char *test_hash[] = {			"CAPITAL?"	, 	"16/04/23"	, "abcdfghijklmnopqrstuvwxyz" ,	 "// This is Test #04 !!"	, "fINAl TEST Aem : 10056, 10021" };
+	int 	ans_hash[] 	= {					0				,				-16			,							759			  		  ,						 	321				  	,								18	   						};
 	ntests = 5;
 	npassed = 0;
 	
@@ -57,10 +57,11 @@ int main(void){
 	
 	#endif
 	
+	//Tests for drootfactorial
 	#ifdef TEST_DRF
-	int  test_drf[] 	= {0, 			 9, 	1023, 	1427,  	 -61, 	2};			
-	int 	ans_drf[] 	= {1, 	362880,	 	 720,	 	 120,		5040, 			2				 };
-	ntests = 6;
+	int  test_drf[] 	= {0, 	-16, 	759, 	 321,  	   18};			
+	int 	ans_drf[] 	= {1,  5040,	  6,	 720,	 362880};
+	ntests = 5;
 	npassed = 0;
 	
 	printf("*************************** DRF TEST START (%d TESTS) ***************************\n", ntests);
@@ -84,14 +85,11 @@ int main(void){
 	
 	#endif
 	
+	
+	//exit if at least one test fails
 	if(exit) return 1;
 	
 	#endif
-	
-	
-	
-	
-	
 	
 	/*MAIN CODE*/
 	
@@ -102,6 +100,7 @@ int main(void){
 	//read user input
 	printf("Please input a string (Less than %d characters)\n", MAXSIZE);
 
+	//Read user input
 	char c;
 	int len = 0;
 	
@@ -114,22 +113,24 @@ int main(void){
 	
 	user_input = realloc(user_input, len * sizeof(char));
 	
-	//start the process
-	
+	//Start calculating
 	printf("Your string is: %s\n", user_input);
-	
-	
+		
 	hash_result = hash(user_input, table);
 	printf("Hash result is: %d\n", hash_result);
 	
 	drf_result = drootfactorial(hash_result);
 	printf("%d 's factorial of its digital root is: %d \n", hash_result, drf_result);
+	
+	//print in UART
 	char result[9];
+	
 	uart_print("My M4 calculates the answer... \r\n");
 	uart_print("The answer is: \r\n");
 	
 	sprintf(result, "%d \r\n", drf_result);
 	uart_print(result);
+	
 	//free memory
 	free(user_input);
 	
